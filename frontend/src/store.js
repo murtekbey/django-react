5e5cfcfcb265e44f45fd3cf00df9e2d945a7b9ab
murtekbey <murtekbey@gmail.com>
2023-10-06 22:33:23 +0300

    shopping cart implementation

---
+++ frontend/src/store.js
@@ -0,0 +1,38 @@
+import {
+  configureStore,
+  combineReducers,
+  applyMiddleware,
+} from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  productListReducers,
+  productDetailsReducers,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+
+const reducers = combineReducers({
+  productList: productListReducers,
+  productDetails: productDetailsReducers,
+
+  cart: cartReducer,
+});
+
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+  },
+};
+
+const middleware = [thunk];
+
+const store = configureStore(
+  { reducer: reducers, preloadedState: initialState },
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
